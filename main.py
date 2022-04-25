96e63a4d7849ac1d9881389aa82422551eb8563a
yashj1212 <yash.jani28@gmail.com>
2022-04-25 17:30:39 +0530

    added looping to the game

--- main.py
+++ main.py
@@ -5,34 +5,35 @@ rps = {1: 'rock', 2: 'paper', 3: 'scissor'}
 computer_choice = int(randint(1, 3))
 # print(computer_choice)
 game_mode = input("Choose game more(r: Random, i: Impossible): ")
-user_choice = int(input("Choose your move(1: Rock, 2: Paper, 3: Scissor): "))
+loop = True
+while loop:
+    user_choice = int(input("Choose your move(1: Rock, 2: Paper, 3: Scissor): "))
 
+    if game_mode.lower() == "r":
+        def normalcomputer(computer_input, user_input):
+            if computer_input == user_input:
+                return 'Tie'
+            elif computer_input == 1 and user_input == 2 or computer_input == 2 and user_input == 1:
+                return 'Paper'
+            elif computer_input == 1 and user_input == 3 or computer_input == 3 and user_input == 1:
+                return 'Rock'
+            elif computer_input == 2 and user_input == 3 or computer_input == 3 and user_input == 2:
+                return 'Scissor'
 
-if game_mode.lower() == "r":
-    def normalcomputer(computer_input, user_input):
-        if computer_input == user_input:
-            return 'Tie'
-        elif computer_input == 1 and user_input == 2 or computer_input == 2 and user_input == 1:
-            return 'Paper'
-        elif computer_input == 1 and user_input == 3 or computer_input == 3 and user_input == 1:
-            return 'Rock'
-        elif computer_input == 2 and user_input == 3 or computer_input == 3 and user_input == 2:
-            return 'Scissor'
+        if normalcomputer(computer_choice, user_choice).lower() == rps[user_choice].lower():
+            print(f"Computer chose {rps[computer_choice]}  You win")
+        elif normalcomputer(computer_choice, user_choice) == 'Tie':
+            print('Tie')
+        else:
+            print(f"Computer chose {rps[computer_choice]} and won!")
 
-    if normalcomputer(computer_choice, user_choice).lower() == rps[user_choice].lower():
-        print(f"Computer chose {rps[computer_choice]}  You win")
-    elif normalcomputer(computer_choice, user_choice) == 'Tie':
-        print('Tie')
-    else:
-        print(f"Computer chose {rps[computer_choice]} and won!")
-
-elif game_mode.lower() == "i":
-        if user_choice == 1:
-            print("Computer chose Paper and Won")
-        elif user_choice == 2:
-            print("Computer chose Scissor and Won")
-        elif user_choice == 3:
-            print("Computer chose Rock and Won")
+    elif game_mode.lower() == "i":
+            if user_choice == 1:
+                print("Computer chose Paper and Won")
+            elif user_choice == 2:
+                print("Computer chose Scissor and Won")
+            elif user_choice == 3:
+                print("Computer chose Rock and Won")
 
 
 
